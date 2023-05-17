aa873367789e7bd283674ab965f8c3f60833bce1
abanu <alex.banu14@gmail.com>
2023-05-17 21:45:53 +0300

    Real inital commit

---
+++ src/test/java/com/lamborghini/urus/UrusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lamborghini.urus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
